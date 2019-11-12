427786031bc5a06c26ff26ded3e9c3b5e33e47bb
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2019-11-12 00:18:14 -0700

    Added server files

--- app.py
+++ app.py
@@ -12,16 +12,33 @@ from views.room_view import blueprint as room_view
 
 app = Flask(__name__)
 
-app.register_blueprint(create_view)
-app.register_blueprint(index_view)
-app.register_blueprint(join_view)
-app.register_blueprint(room_view)
-app.register_blueprint(card_view)
 
-folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.insert(0, folder)
-db_file = os.path.join(os.path.dirname(__file__), 'db', 'treachery.sqlite')
-db_session.global_init(db_file)
+def register_blueprints():
+    app.register_blueprint(create_view)
+    app.register_blueprint(index_view)
+    app.register_blueprint(join_view)
+    app.register_blueprint(room_view)
+    app.register_blueprint(card_view)
+
+
+def setup_db():
+    folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    sys.path.insert(0, folder)
+    db_file = os.path.join(os.path.dirname(__file__), 'db', 'treachery.sqlite')
+    db_session.global_init(db_file)
+
+
+def configure():
+    register_blueprints()
+    setup_db()
+
+
+def main():
+    configure()
+    app.run(debug=True)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    main()
+else:
+    configure()
